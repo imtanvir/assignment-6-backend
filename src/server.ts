c7b6af322ed40651850591b082edacf061fad881
imtanvir <tanvirparvej101@gmail.com>
2024-10-16 05:06:05 +0600

    initial project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+async function main() {
+  //   await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.PORT, () => {
+      console.log(`Pet Care app listening on port ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
