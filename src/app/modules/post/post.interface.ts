8de363c4154eba9553c1e1f8d681d2644dd18abe
imtanvir <tanvirparvej101@gmail.com>
2024-10-19 07:38:46 +0600

    post interface declared

---
+++ src/app/modules/post/post.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+export type TCategory = 'Tip' | 'Story';
+export type TComment = {
+  author: Types.ObjectId;
+  comment: string;
+  postId: Types.ObjectId;
+};
+export type TPost = {
+  user: Types.ObjectId;
+  id: Types.ObjectId;
+  premium: boolean;
+  category: TCategory;
+  comment: TComment[] | null;
+};
